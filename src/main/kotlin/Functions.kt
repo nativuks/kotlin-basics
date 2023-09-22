1070cd8b16a9883f29a2c7044687941cc650af26
Fernando Sejas <nativuks@gmail.com>
2023-09-22 06:53:22 -0400

    Restore File

---
+++ src/main/kotlin/Functions.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val hobbits = arrayOf("Frodo", "Sam", "Merry", "Pippin")
+    var x = 0
+
+    while(x < 4) {
+        println("${hobbits[x]} is a good Hobbit name")
+        x ++
+    }
+}
