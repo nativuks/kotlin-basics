39ed53365445012ee49084f2ad63336ab7357d2d
Fernando Sejas <nativuks@gmail.com>
2023-09-22 06:50:33 -0400

    delete content Functions file

--- src/main/kotlin/Functions.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val hobbits = arrayOf("Frodo", "Sam", "Merry", "Pippin")
-    var x = 0
-
-    while(x < 4) {
-        println("${hobbits[x]} is a good Hobbit name")
-        x ++
-    }
-}
